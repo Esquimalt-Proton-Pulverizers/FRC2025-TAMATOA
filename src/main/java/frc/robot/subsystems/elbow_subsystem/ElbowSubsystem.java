9bc6c572a708ab069fb744caf40caa0a9ff59217
Esquimalt Proton Pulverizers <esqprotonpulverizers@gmail.com>
2025-07-14 23:00:59 -0700

    To be solved: control complexity with rotation

--- src/main/java/frc/robot/subsystems/elbow_subsystem/ElbowSubsystem.java
+++ src/main/java/frc/robot/subsystems/elbow_subsystem/ElbowSubsystem.java
@@ -22,8 +22,8 @@ public class ElbowSubsystem extends SubsystemBase{
     public final static double START_POS_ROTATION = 0;
 
     public final static double HORIZONTAL_POS_ELEVATION = -90;
-    public final static double HORIZONTAL_POS_ROTATION = 0;// to be 90
-    public final static double CORAL_COMPENSATION = 0;//to be 90
+    public final static double HORIZONTAL_POS_ROTATION = 90;
+    public final static double CORAL_COMPENSATION = 45;
 
     public final static double INTAKE_POS_ELEVATION = -120;
     public final static double INTAKE_POS_ROTATION = 0;// to be 90
