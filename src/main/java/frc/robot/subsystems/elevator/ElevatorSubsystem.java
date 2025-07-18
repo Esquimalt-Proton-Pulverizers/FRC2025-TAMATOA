c3d1a34a4a14a2b374bc72e079125888cb0dd388
Hanson2258 <47832494+Hanson2258@users.noreply.github.com>
2025-07-18 15:11:15 -0700

    Changed elevator low and starting position

--- src/main/java/frc/robot/subsystems/elevator/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorSubsystem.java
@@ -21,7 +21,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class ElevatorSubsystem extends SubsystemBase {
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
-  public static final double LOW_POSITION=0.0;
+  public static final double LOW_POSITION=1.0;
   //public static final double processorPosition=10;
   public static final double LEVEL1_POSITION=10.0;
   public static final double LEVEL2_POSITION=14.5;
@@ -73,7 +73,7 @@ public class ElevatorSubsystem extends SubsystemBase {
         
 
     elevatorMotor.configure(elevatorConfig, ResetMode.kResetSafeParameters, PersistMode.kNoPersistParameters);
-    elevatorClosedLoopController.setReference(0, SparkMax.ControlType.kPosition);    
+    elevatorClosedLoopController.setReference(LOW_POSITION, SparkMax.ControlType.kPosition);    
   }
 
   
