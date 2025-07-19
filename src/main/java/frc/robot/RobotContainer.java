e4a9194c612e25ffb4625bb9be11c329ab0fdbca
Hanson2258 <47832494+Hanson2258@users.noreply.github.com>
2025-07-19 11:16:42 -0700

    NEEDS TESTING - Basic Auto drive forward program

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -9,7 +9,12 @@ import static edu.wpi.first.units.Units.*;
 import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
 import com.ctre.phoenix6.swerve.SwerveRequest;
 
+import com.pathplanner.lib.auto.AutoBuilder;
+
+
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
@@ -48,8 +53,14 @@ public class RobotContainer {
 
     public final HangingSubsystem hanger = new HangingSubsystem();
 
+	// Path follower
+	private final SendableChooser<Command> autoChooser;
+
     public RobotContainer() {
+		registerCommands();
         configureBindings();
+		autoChooser = AutoBuilder.buildAutoChooser("MoveOnly");
+		SmartDashboard.putData("Auto Mode", autoChooser);
     }
 
     private void configureBindings() {
@@ -89,7 +100,12 @@ public class RobotContainer {
         joystick.leftBumper().onFalse(hanger.stop());
     }
 
-    public Command getAutonomousCommand() {
-        return Commands.print("No autonomous command configured");
-    }
+	public Command getAutonomousCommand() {
+		/* Run the path selected from the auto chooser */
+		return autoChooser.getSelected();
+	}
+
+    private void registerCommands() {
+		// Register the commands here
+	}
 }
