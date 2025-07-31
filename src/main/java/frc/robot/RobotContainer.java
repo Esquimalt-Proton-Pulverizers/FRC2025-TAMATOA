9b2d717fa5fc9df17aecd2649f4d8b341f7f3beb
Morrisinio123 <colinm1208@gmail.com>
2025-07-31 14:42:50 -0700

    added a new command to move elevator to 20 inches (button 9)

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -159,7 +159,7 @@ public class RobotContainer {
         //// --------------- Elevator Commands ---------------
         operatorController.button(11).onTrue(Commands.runOnce(()->elevatorSubsystem.manualMove(ELEVATOR_MOVEMENT_PER_CLICK), elevatorSubsystem));  // Left Stick Button
         operatorController.button(12).onTrue(Commands.runOnce(()->elevatorSubsystem.manualMove(-ELEVATOR_MOVEMENT_PER_CLICK), elevatorSubsystem)); // Right Stick Button
-
+		operatorController.button(9).onTrue(new ElevatorToPosCommand(20.0, elevatorSubsystem, false, false));    // DPad Up
 	}
 
 	public Command getAutonomousCommand() {
